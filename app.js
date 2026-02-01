bac8b4ac492b318641d76c2167cab0d84150e086
Leo <184458875+leocoding0326@users.noreply.github.com>
2026-01-31 19:59:00 -0600

    Completed img zoom in functionality
    when img is clicked it will zoom in 80%
    when the user clicks out of the image it will close

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+
+//Burger Menu Opening and Closing//
 const burgerBtn = document.querySelector('.burger-btn');
-const overlay = document.querySelector('#overlay');
 
 const  setMenu = (open) => {
     document.body.classList.toggle('menu-open', open)
@@ -8,4 +9,24 @@ const  setMenu = (open) => {
 burgerBtn.addEventListener('click', () => {
     const open = document.body.classList.contains('menu-open')
     setMenu(!open)
+})
+
+//Zoom In Image Trigger //
+
+const zoomable = document.querySelector('.zoomable')
+const overlay = document.querySelector('.zoom-overlay')
+
+const zoomIn = zoom => {
+    document.body.classList.toggle('zoom-in', zoom)
+}
+
+zoomable.addEventListener('click', () => {
+    const zoom = document.body.classList.contains('zoom-in')
+    zoomIn(!zoom)
+})
+
+overlay.addEventListener('click', (event) => {
+    if (event.target === event.currentTarget){
+    zoomIn(false)
+    }
 })
